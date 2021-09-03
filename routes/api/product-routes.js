69db7d620185d72fde6dde5e240352bc657da051
Mohammad Zafar <sz7861@outlook.com>
2021-09-03 12:52:15 +0100

    Find products by id, including category and tag

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Tag, through: ProductTag, as:'tag_it'}, { model: Category }]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Tag, through: ProductTag, as:'tag_it'}, { model: Category }]    
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No tag matches id' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
