2aa2a7026b257f715e6b81c1bc429c613d741a2b
Mohammad Zafar <sz7861@outlook.com>
2021-09-03 12:24:43 +0100

    Find categories + associated products

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,15 +2,34 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+});
+res.status(200).json(categoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category matches this id' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
