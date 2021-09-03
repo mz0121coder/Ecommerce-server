73eb53d3b5533cc64db9a43b0d9344c06e73d223
Mohammad Zafar <sz7861@outlook.com>
2021-09-03 12:36:42 +0100

    Create/update/delete categories by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -23,7 +23,7 @@ router.get('/:id', (req, res) => {
       include: [{ model: Product }]
     });
     if (!categoryData) {
-      res.status(404).json({ message: 'No category matches this id' });
+      res.status(404).json({ message: 'No category matches id' });
       return;
     }
     res.status(200).json(categoryData);
@@ -34,14 +34,49 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      }, 
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No category matches id' });
+      return; 
+    }
+    res.json(categoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category matches id' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
